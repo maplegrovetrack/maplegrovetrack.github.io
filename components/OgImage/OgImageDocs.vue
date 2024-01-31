96c2e4a6f9f6498e7e92b72f8cb5532e5c57ccf8
Andrew Norman <Andrew.C.Norman@HealthPartners.Com>
2024-01-30 19:50:26 -0600

    Update og image docs.

--- components/OgImage/OgImageDocs.vue
+++ components/OgImage/OgImageDocs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full flex flex-col justify-center p-8 bg-stone-900">
-    <img class="absolute h-72 top-1.5 right-1.5" src="/logo_leaf.png" alt="Maple Grove Senior High Maple Leaf" />
+    <img class="absolute w-72 top-1.5 right-1.5" src="/logo_leaf.png" alt="Maple Grove Senior High Maple Leaf" />
     <div class="relative">
       <h1 class="text-8xl mb-4 text-white">
         {{ title }}
