f290ce7d49fe5ef42aaa57f6a885b9ad4ca87759
anormy <53493826+anormy@users.noreply.github.com>
2025-03-04 22:47:51 -0600

    Records faq (#53)
    * Added records
    
    * Reconfigure links a bit.
    
    * Updates.
    
    * updates.
    
    * Minor updates.
    
    ---------
    
    Co-authored-by: Andrew Norman <Andrew.C.Norman@HealthPartners.Com>

--- composables/events/1.use-mon-mar-10-2025.ts
+++ composables/events/1.use-mon-mar-10-2025.ts
@@ -10,7 +10,7 @@ export const useMonMar1020205Event = (): Event => ({
   notes: [
     'The MGTF Booster Club is hosting a Community Sponsor Night in conjunction with Omni on 3/10/2025.',
     '- Omni will donate $1 for each Coffee, Beverage, or Pizza served that day 7am – 10pm. Consider grabbing a coffee to start your day or do a pizza in the evening.',
-    '- Be sure to join from 5-8pm as a fun way to get to know other track and field parents. Booster members will be there 5-8pm to answer any questions and to start the community build we are excited about in 2025!',
+    '- Be sure to join from 5-8pm as a fun way to get to know other track & field parents. Booster members will be there 5-8pm to answer any questions and to start the community build we are excited about in 2025!',
     'This will be the kickoff to some fun opportunities to build MGTF program through awesome community sponsors.',
     'Unlike other sports teams, we don\'t require fundraising but hope you will share the attached Community Night flyer (in stack teamp app) on social media, text, or email.',
     'We welcome you to join us in additional opportunities by checking out the attached pdf (in stack team app) on how to "Support Our Program".',
