8040c3cfd38192ddafd6e9c0a8a473805e674890
Andrew Norman <Andrew.C.Norman@HealthPartners.Com>
2026-01-08 23:25:16 -0600

    Last update.

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -1,4 +1,5 @@
 import type { H3Event } from 'h3'
+import { queryCollectionSearchSections } from '@nuxt/content/server'
 
 export default defineEventHandler(async (event: H3Event) => {
   return queryCollectionSearchSections(event, 'site')
