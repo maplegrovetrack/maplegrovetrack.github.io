e6416ac72ac8424557f554bb4803599f983275b0
Andrew Norman <Andrew.C.Norman@HealthPartners.Com>
2026-02-21 11:59:42 -0600

    Added meets.

--- app/app.vue
+++ app/app.vue
@@ -24,7 +24,12 @@
 import type { ContentNavigationItem } from '@nuxt/content'
 
 const navigation: ContentNavigationItem[] = await queryCollectionNavigation('site')
-const nav = [{ title: 'Site', path: '/', stem: '0.index', children: navigation }]
+// For some reason adding directories removes the root pages from search results.
+// Below setup is a workaround to show root pages in the search results.
+const nav = [
+  { title: '', path: '/', stem: '0.index', children: navigation.filter(nav => nav && !nav.children) },
+  ...navigation.filter(nav => nav && nav.children)
+]
 const { data: files } = await useLazyFetch('/api/search.json', {
   default: () => [],
   server: false
