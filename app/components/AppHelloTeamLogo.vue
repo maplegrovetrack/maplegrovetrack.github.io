c5d063cc78f8d5f9a7f47955730f7dc6044329d0
Andrew Norman <Anormy123@gmail.com>
2026-03-01 14:36:24 -0600

    Added stack app.

---
+++ app/components/AppHelloTeamLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <UButton
+    to="https://zfrmz.com/EOrq3g1LfQWTzdKj7Nwt"
+    target="_blank"
+    aria-label="Maple Grove Track & Field Hello Team"
+    color="neutral"
+    variant="ghost"
+  >
+    <img src="/hello-team-logo.webp" alt="Hello Team Logo" class="h-5 w-5">
+  </UButton>
+</template>
+
+<script lang="ts" setup>
+</script>
